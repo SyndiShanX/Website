3448379ec6a865f2a8aa024df025e57fd444e2ac
SyndiShanX <brianmatthew.1026@gmail.com>
2022-06-24 02:50:53 -0400

    Updated Shi No Numa Guide, and Overhauled Backend Layout
    Moved Zombies Maps into their own Folders, and added a WIP Selection Page

--- GlobalNavbar.js
+++ GlobalNavbar.js
@@ -23,6 +23,7 @@ var table = document.createElement('table')
 var tbody = document.createElement('tbody')
 tbody.className = 'nav-items'
 var tr = document.createElement('tr')
+tr.className = 'navTR'
 topnav.appendChild(logo)
 topnav.appendChild(logoImg)
 topnav.appendChild(table)
@@ -36,6 +37,7 @@ var tempA = ''
 
 function createNavItem(classname, textContent, href) {
 	tempTD = document.createElement('td')
+	tempTD.className = 'navTD'
 	tempA = document.createElement('a')
 	tempA.className = 'nav-item ' + classname
 	tempA.textContent = textContent
@@ -52,6 +54,7 @@ var tempDropdownDiv1 = ''
 
 function createNavDropdown(classname, textContent) {
 	tempDropdownTD = document.createElement('td')
+	tempDropdownTD.className = 'navTD'
 	tempDropdownDiv = document.createElement('div')
 	tempDropdownDiv.className = 'dropdown'
 	tempDropdownA = document.createElement('a')
@@ -91,7 +94,8 @@ function createNavDropdownItems(itemNum, dropdownClassname, textContentArray, hr
 
 createNavItem('home', 'Home', '/Website/')
 createNavItem('minesweeper', 'Minesweeper', prepend + 'Minesweeper/Minesweeper.html')
-createNavItem('dokustashGenerator end', 'Dokustash Generator', prepend + 'Dokustash_Generator.html')
+createNavItem('dokustashGenerator', 'Dokustash Generator', prepend + 'Dokustash_Generator.html')
+//createNavItem('zombies end', 'Zombies', prepend + 'Zombies/Selector.html')
 
 /* Nav Dropdown Menus */
 
@@ -99,24 +103,28 @@ createNavDropdown('vanguard', 'Vanguard')
 createNavDropdown('coldwar', 'Cold War')
 createNavDropdown('bo4', 'Black Ops 4')
 createNavDropdown('bo3', 'Black Ops 3')
+//createNavDropdown('zombies', 'Zombies')
 createNavDropdown('more', 'More')
 
 /* Nav Dropdown Items */
 
-createNavDropdownItems(3, 'vanguardDropdown', ['Der Anfang', 'Terra Maledicta', 'Shi No Numa'], ['Vanguard/Der_Anfang.html', 'Vanguard/Terra_Maledicta.html', 'Vanguard/Shi_No_Numa.html'])
-createNavDropdownItems(6, 'coldwarDropdown', ['Die Maschine', 'Firebase Z', 'Mauer Der Toten', 'Forsaken', 'Outbreak'], ['Cold_War/Die_Maschine.html', 'Cold_War/Firebase_Z.html', 'Cold_War/Mauer_Der_Toten.html', 'Cold_War/Forsaken.html', 'Cold_War/Outbreak.html'])
-createNavDropdownItems(12, 'bo4Dropdown', ['Voyage of Despair', 'Voyage of Despair Tool', 'IX', 'Blood of The Dead', 'Classified', 'Dead of The Night', 'Stake Knife', 'Ancient Evil', 'Alpha Omega', 'Alpha Omega Tool', 'Tag Der Toten', 'Random Map'], ['BlackOps4/Voyage_of_Despair.html', 'BlackOps4/Voyage_of_Despair_Tool.html', 'BlackOps4/IX.html', 'BlackOps4/Blood_of_The_Dead.html', 'BlackOps4/Classified.html', 'BlackOps4/Dead_of_The_Night.html', 'BlackOps4/Stake_Knife.html', 'BlackOps4/Ancient_Evil.html', 'BlackOps4/Alpha_Omega.html', 'BlackOps4/Alpha_Omega_Tool.html', 'BlackOps4/Tag_Der_Toten.html', 'BlackOps4/Random_Map.html'])
-createNavDropdownItems(7, 'bo3Dropdown', ['Shadows of Evil', 'Shadows of Evil Tool', 'The Giant', 'Der Eisendrache', 'Zetsubou No Shima', 'Gorod Krovi', 'Revelations'], ['BlackOps3/Shadows_of_Evil.html', 'BlackOps3/Shadows_of_Evil_Tool.html', 'BlackOps3/The_Giant.html', 'BlackOps3/Der_Eisendrache.html', 'BlackOps3/Zetsubou_No_Shima.html', 'BlackOps3/Gorod_Krovi.html', 'BlackOps3/Revelations.html'])
-createNavDropdownItems(5, 'moreDropdown', ['The Final Reich', 'Dokucraft Banner Editor', 'Console Dokucraft', 'Downloads'], ['The_Final_Riech.html', 'DokucraftBannerEditor/Dokucraft_Banner_Editor.html', 'Downloads/Console_Dokucraft.html', 'Downloads/Downloads.html'])
+createNavDropdownItems(3, 'vanguardDropdown', ['Der Anfang', 'Terra Maledicta', 'Shi No Numa'], ['Zombies/Vanguard/Der_Anfang.html', 'Zombies/Vanguard/Terra_Maledicta.html', 'Zombies/Vanguard/Shi_No_Numa.html'])
+createNavDropdownItems(5, 'coldwarDropdown', ['Die Maschine', 'Firebase Z', 'Mauer Der Toten', 'Forsaken', 'Outbreak'], ['Zombies/ColdWar/Die_Maschine.html', 'Zombies/ColdWar/Firebase_Z.html', 'Zombies/ColdWar/Mauer_Der_Toten.html', 'Zombies/ColdWar/Forsaken.html', 'Zombies/ColdWar/Outbreak.html'])
+createNavDropdownItems(12, 'bo4Dropdown', ['Voyage of Despair', 'Voyage of Despair Tool', 'IX', 'Blood of The Dead', 'Classified', 'Dead of The Night', 'Stake Knife', 'Ancient Evil', 'Alpha Omega', 'Alpha Omega Tool', 'Tag Der Toten', 'Random Map'], ['Zombies/BlackOps4/Voyage_of_Despair.html', 'Zombies/BlackOps4/Voyage_of_Despair_Tool.html', 'Zombies/BlackOps4/IX.html', 'Zombies/BlackOps4/Blood_of_The_Dead.html', 'Zombies/BlackOps4/Classified.html', 'Zombies/BlackOps4/Dead_of_The_Night.html', 'Zombies/BlackOps4/Stake_Knife.html', 'Zombies/BlackOps4/Ancient_Evil.html', 'Zombies/BlackOps4/Alpha_Omega.html', 'Zombies/BlackOps4/Alpha_Omega_Tool.html', 'Zombies/BlackOps4/Tag_Der_Toten.html', 'Zombies/BlackOps4/Random_Map.html'])
+createNavDropdownItems(7, 'bo3Dropdown', ['Shadows of Evil', 'Shadows of Evil Tool', 'The Giant', 'Der Eisendrache', 'Zetsubou No Shima', 'Gorod Krovi', 'Revelations'], ['Zombies/BlackOps3/Shadows_of_Evil.html', 'Zombies/BlackOps3/Shadows_of_Evil_Tool.html', 'Zombies/BlackOps3/The_Giant.html', 'Zombies/BlackOps3/Der_Eisendrache.html', 'Zombies/BlackOps3/Zetsubou_No_Shima.html', 'Zombies/BlackOps3/Gorod_Krovi.html', 'Zombies/BlackOps3/Revelations.html'])
+//createNavDropdownItems(9, 'zombiesDropdown', ['Black Ops 1', 'Black Ops 2', 'Advanced Warfare', 'Black Ops 3', 'Infinite Warfare', 'World War 2', 'Black Ops 4', 'Cold War', 'Vanguard'], ['Zombies/BlackOps1/Selection.html', 'Zombies/BlackOps2/Selection.html','Zombies/AW/Selection.html','Zombies/BlackOps3/Selection.html', 'Zombies/IW/Selection.html', 'Zombies/WW2/Selection.html', 'Zombies/BlackOps4/Selection.html', 'Zombies/ColdWar/Selection.html', 'Zombies/Vanguard/Selection.html'])
+createNavDropdownItems(4, 'moreDropdown', ['The Final Reich', 'Dokucraft Banner Editor', 'Console Dokucraft', 'Downloads'], ['Zombies/WW2/The_Final_Riech.html', 'DokucraftBannerEditor/Dokucraft_Banner_Editor.html', 'Downloads/Console_Dokucraft.html', 'Downloads/Downloads.html'])
 
 /* Nav Finalization */
 
 tr.appendChild(spacer)
 
 var spacerTD = document.createElement('td')
+spacerTD.className = 'navTD'
 spacerTD.appendChild(spacerSmall)
 tr.appendChild(spacerTD)
 var spacerTD2 = document.createElement('td')
+spacerTD2.className = 'navTD'
 spacerTD2.appendChild(spacerSmall2)
 tr.appendChild(spacerTD2)
 
